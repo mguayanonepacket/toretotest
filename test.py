0ee734fc86e851dd7bf96e4bb7dbdeded45552ac
Mario Guayanone <94442068+mguayanonepacket@users.noreply.github.com>
2024-10-03 12:43:50 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
+from tqdm import tqdm
+import time
+ 
 print('hello torero')
+
+ 
+for i in tqdm (range (101), 
+               desc="Loading…", 
+               ascii=False, ncols=75):
+    time.sleep(0.01)
+     
+print("Complete.")
