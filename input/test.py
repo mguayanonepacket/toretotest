5262d00e0b630b07d49e318342d2af4dce20808d
mguayanonepacket <mario.guayanone@packet.com>
2024-10-03 12:19:43 +0000

    quitando print

--- input/test.py
+++ input/test.py
@@ -1,14 +1,2 @@
-from tqdm import tqdm
-import time 
-import requests
-
-def test():
-    x=input('cual es tu nombre:')
-    print(f'tu nombre es {x}')
-
-def main():
-    test()
-
-if __name__ == "__main__":
-    main()
-
+x=input('who are you?:')
+print(f'you are {x}')
