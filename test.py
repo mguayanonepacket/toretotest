2810b012ddc0d5eef8278eaae6fc6aa5d1c659e2
Mario Guayanone <94442068+mguayanonepacket@users.noreply.github.com>
2024-10-03 12:59:08 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+from tqdm import tqdm
+import time 
 import requests
 
 def check_requests_version():
@@ -7,8 +9,21 @@ def check_requests_version():
     else:
         print("La versión de requests no es la correcta.")
 
+def test():
+	print('hello torero')
+	 
+	for i in tqdm (range (101), 
+	               desc="Loading…", 
+	               ascii=False, ncols=75):
+	    time.sleep(0.01)
+	     
+	print("Complete.")
+
 def main():
     check_requests_version()
-
+    test()
+	     
+print("Complete.")
 if __name__ == "__main__":
     main()
+
