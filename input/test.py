c877ad479ed94b8657aba9c2b4c3204e05fd8ae3
mguayanonepacket <Mario.guayanone@eu.equinix.com>
2024-10-03 13:25:46 +0200

    probando input

---
+++ input/test.py
@@ -0,0 +1,15 @@
+from tqdm import tqdm
+import time 
+import requests
+
+def test():
+    x=input('cual es tu nombre:')
+    print(f'tu nombre es {x}')
+
+def main():
+    test()
+	     
+print("Complete.")
+if __name__ == "__main__":
+    main()
+
