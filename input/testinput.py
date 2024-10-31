ad0e35bee4bac79f1be23305143b259b30178952
Mario Guayanone <94442068+mguayanonepacket@users.noreply.github.com>
2024-10-31 09:34:16 +0100

    Update testinput.py

--- input/testinput.py
+++ input/testinput.py
@@ -1,2 +1,11 @@
+import sys
+
+if len(sys.argv) > 1:
+    # Imprimir los argumentos
+    for arg in sys.argv[1:]:
+        print(arg)
+else:
+    print("No se pasaron argumentos.")
+
 x=input('who are you?:')
 print(f'you are {x}')
